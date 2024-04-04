96af60858a451804249975e75b5d0e8953b0001f
Jobson Almeida <jobsonbox@gmail.com>
2024-04-04 14:24:22 -0300

    Configured EditAluno.view to show the spaces of the four class

--- js/Views/EditAluno.view.js
+++ js/Views/EditAluno.view.js
@@ -3,55 +3,70 @@ class EditAlunoView{
     constructor(container, materias){
         // this.render()
         this.container = container
+        this.materias = materias
 
     }
 
     render(aluno){
 
-        const html = `
-        
-        <div class="row">        
+        const html = this.materias.map( materia =>
 
-        <div class="input-field col s4">
-          <input
-            id="materia"
-            type="text"
-            class="validate"
-          />
-        </div>
-        <div class="input-field col s2">
-          <input
-            id="nota_materia_1"
-            type="text"
-            class="validate"
-          />
-        </div>
-        <div class="input-field col s2">
-          <input
-            id="nota_materia_2"
-            type="text"
-            class="validate"
-          />
-        </div>
-        <div class="input-field col s2">
-          <input
-            id="nota_materia_3"
-            type="text"
-            class="validate"
-          />
-        </div>
-        <div class="input-field col s2">
-          <input
-            id="nota_materia_4"
-            type="text"
-            class="validate"
-          />
-        </div>
-      </div> 
+          `
         
-        `
+          <div class="row">        
+  
+          <div class="input-field col s4">
+            <input
+              id="materia_${materia}"
+              type="text"
+              class="validate"
+              value="${materia}"
+              disabled
+            />
+          </div>
+  
+          <div class="input-field col s2">
+            <input
+              id="nota_materia_1"
+              type="text"
+              class="validate"
+            />
+          </div>
+  
+          <div class="input-field col s2">
+            <input
+              id="nota_materia_2"
+              type="text"
+              class="validate"
+            />
+          </div>
+  
+          <div class="input-field col s2">
+            <input
+              id="nota_materia_3"
+              type="text"
+              class="validate"
+            />
+          </div>
+  
+          <div class="input-field col s2">
+            <input
+              id="nota_materia_4"
+              type="text"
+              class="validate"
+            />
+          </div>
+  
+        </div> 
+          
+          `
 
-        this.container.innerHTML = html
+        ).join("")
+        
+       
+        this.container.innerHTML = html  
+        
+        // console.log(this.container.innerHTML)
 
     }
 
