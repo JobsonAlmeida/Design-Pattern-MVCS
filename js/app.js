86575c88e2c2704896dbbb73a997dc4ef0bf9c96
Jobson Almeida <jobsonbox@gmail.com>
2024-04-04 11:15:03 -0300

    Created Model, Controllers, Services and Models. Created index and edit files.

---
+++ js/app.js
@@ -0,0 +1,44 @@
+
+const alunosOld = [
+    {
+        _id: 0,
+        nome: "chico melato",
+        notas: {
+        portugues: [1, 1, 2, 2],
+        matematica: [2, 2, 2, 2],
+        historia: [5, 5, 5, 5],
+        ciencias: [3, 3, 3, 3],
+        },
+    },
+    {
+        _id: 1,
+        nome: "talita lima",
+        notas: {
+        portugues: [10, 10, 10, 10],
+        matematica: [4, 4, 5, 5],
+        historia: [5, 5, 6, 6],
+        ciencias: [7, 7, 8, 9],
+        },
+    },
+  ];
+    
+const alunosService = new AlunosService()
+
+// alunos.forEach(aluno => {
+//     alunosService.add(new AlunoModel(aluno))  
+// });
+
+const alunosView = new AlunosView(document.querySelector("[data-table-alunos]"), new MateriasService().materias)
+
+const alunosController = new AlunosController(alunosService, alunosView)
+
+document.querySelector("form").addEventListener("submit", e =>{
+    e.preventDefault()
+
+    const nome = document.getElementById("first_name").value
+    console.log(nome)
+    console.log(typeof nome)
+
+    alunosController.add({nome})
+
+})
